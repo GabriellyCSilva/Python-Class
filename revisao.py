760aaafdea62f16bceb0a2c8a9c36ca584877493
Gabrielly Candido <gabriellycasilva@gmail.com>
2025-02-17 14:19:51 -0300

    Add files via upload

---
+++ revisao.py
@@ -0,0 +1,60 @@
+#concatenação delas com exceção do primeiro caractere de cada uma.
+"""palavra3 = "Gabrielly"
+palavra4= "Fiap"
+print(palavra3[1:]+ palavra4[1:])"""
+from unicodedata import numeric
+
+#concatenação de três cópias dos dois últimos caracteres.
+"""palavra5= "Gabrielly"
+print((palavra5[-2:])*3)"""
+
+#Faça uma tabela mostrando os valores que i, j e n assumem depois de cada execução do laço while:
+
+"""i=0
+j=10
+n=0
+while i<j:
+    print(i, j, n)
+    i=i+1
+    j=j-1
+    n=n+1"""
+
+#Faça um programa para exibiros números de 1 a 100
+"""numero = 0
+while numero <= 100:
+    print(numero)
+    numero+=1"""
+
+
+#Faça um programa para exibiros números de 50 a 100
+"""numero = 50
+while numero <= 100:
+    print(numero)
+    numero+=1"""
+
+#programa para escrever a contagem regressiva do lançamento de um foguete. O programa deve imprimir 10, 9, 8, …, 1, 0 e Fogo! na tela.
+"""numero= 10
+while numero <= 10 and numero >= 0:
+    print(numero)
+    numero = numero - 1
+print("Fogo!")"""
+
+
+#Faça um programa para imprimir de 1 até o número digitado pelo usuário, mostrando apenas os números pares.
+"""num_usuario = int(input("Digite um número: "))
+numero = 1
+while numero <= num_usuario:
+    print(numero)
+    numero+=1"""
+
+
+#Faça um programa para imprimir de 1 até o número digitado pelo usuário, mostrando apenas os números ímpares.
+"""num_usuario = int(input("Digite um número: "))
+numero = 1
+while numero <= num_usuario:
+    if numero%2 != 0:
+        print(numero)
+    numero+=1"""
+
+
+
