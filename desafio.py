735a88ac87018d0d01f2faa05f010ad5a4bda67a
Gabrielly Candido <gabriellycasilva@gmail.com>
2025-02-20 22:40:56 -0300

    Add files via upload

---
+++ desafio.py
@@ -0,0 +1,16 @@
+#mostra numeros aleatorios de 0 a 1
+import random
+a= random.random()
+print(a)
+
+#coloca um delay na hora de importar
+import time
+for i in range (1,11):
+    time.sleep(1)
+    print(i)
+
+#help ajuda a lembrar o que cada um faz
+
+
+
+
